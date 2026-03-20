7194643ee554cfa5e192740e3be2ae46e2b914f6
Stanisław Łapiński <stanislaw.lapinski@proton.me>
2026-03-20 17:33:40 +0100

    Adds clearer description of the Test Cases

--- test_virtual_column 1.py
+++ test_virtual_column 1.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from solution import add_virtual_column
 
+# -------------------- ORIGINAL UNIT TESTS --------------------
 
 def test_sum_of_two_columns():
     df = pd.DataFrame([[1, 1]] * 2, columns = ["label_one", "label_two"])
@@ -36,45 +37,50 @@ def test_empty_result_when_invalid_labels():
     print("\n4) Test 'test_empty_result_when_invalid_labels' ✅")
 
 
-def test_empty_result_when_invalid_labels_from_pdf():
-    df = pd.DataFrame([[1, 2]] * 3, columns = ["label_one", "label_two"])
-    df_result = add_virtual_column(df, "label_one + label_two", "label3")
-    assert df_result.empty, f"5) Should return an empty df when the \"new_column\" is invalid.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
-    df = pd.DataFrame([[1, 2]] * 3, columns = ["label-one", "label_two"])
-    df_result = add_virtual_column(df, "label-one + label_two", "label")
-    assert df_result.empty, f"5) Should return an empty df when both df columns and roles are invalid.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
-    df = pd.DataFrame([[1, 2]] * 3, columns = ["label-one", "label_two"])
-    df_result = add_virtual_column(df, "label_one + label_two", "label")
-    assert df_result.empty, f"5) Should return an empty df when a df column is invalid.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
-    print("\n5) Test 'test_empty_result_when_invalid_labels_from_pdf' ✅")
-
-
 def test_empty_result_when_invalid_rules():
     df = pd.DataFrame([[1, 1]] * 2, columns = ["label_one", "label_two"])
     df_result = add_virtual_column(df, "label&one + label_two", "label_three")
-    assert df_result.empty, f"6) Should return an empty df when the role have invalid character: '&'.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
+    assert df_result.empty, f"5) Should return an empty df when the role have invalid character: '&'.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
     df_result = add_virtual_column(df, "label_five + label_two", "label_three")
-    assert df_result.empty, f"6) Should return an empty df when the role have a column which isn't in the df: 'label_five'.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
-    print("\n6) Test 'test_empty_result_when_invalid_rules' ✅")
+    assert df_result.empty, f"5) Should return an empty df when the role have a column which isn't in the df: 'label_five'.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
+    print("\n5) Test 'test_empty_result_when_invalid_rules' ✅")
 
-def test_empty_result_when_invalid_rules_from_pdf():
-    df = pd.DataFrame([[1, 1]] * 2, columns = ["label_one", "label_two"])
-    df_result = add_virtual_column(df, "label_one \\ label_two", "label_three")
-    assert df_result.empty, f"7) Should return an empty df when the role have invalid character: '\\'.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
-    df_result = add_virtual_column(df, "label&one + label_two", "label_three")
-    assert df_result.empty, f"7) Should return an empty df when the role have invalid character: '&'.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
-    df_result = add_virtual_column(df, "label_five + label_two", "label_three")
-    assert df_result.empty, f"7) Should return an empty df when the role have a column which isn't in the df: 'label_five'.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
-    print("\n7) Test 'test_empty_result_when_invalid_rules_from_pdf' ✅")
 
 def test_when_extra_spaces_in_rules():
     df = pd.DataFrame([[1, 1]] * 2, columns = ["label_one", "label_two"])
     df_expected = pd.DataFrame([[1, 1, 2]] * 2, columns = ["label_one", "label_two", "label_three"])
     df_result = add_virtual_column(df, "label_one + label_two ", "label_three")
-    assert df_result.equals(df_expected), f"8) Should work when the role have spaces between the operation and the column.\n\nResult:\n\n{df_result}\n\nExpected:\n\n{df_expected}"
+    assert df_result.equals(df_expected), f"6) Should work when the role have spaces between the operation and the column.\n\nResult:\n\n{df_result}\n\nExpected:\n\n{df_expected}"
     df_result = add_virtual_column(df, "  label_one + label_two ", "label_three")
-    assert df_result.equals(df_expected), f"8) Should work when the role have extra spaces in the start/end.\n\nResult:\n\n{df_result}\n\nExpected:\n\n{df_expected}"
-    print("\n8) Test 'test_when_extra_spaces_in_rules' ✅")
+    assert df_result.equals(df_expected), f"6) Should work when the role have extra spaces in the start/end.\n\nResult:\n\n{df_result}\n\nExpected:\n\n{df_expected}"
+    print("\n6) Test 'test_when_extra_spaces_in_rules' ✅")
+
+
+# -------------------- UNIT TESTS I FOUND IN PDF --------------------
+
+def test_empty_result_when_invalid_labels_from_pdf():
+    df = pd.DataFrame([[1, 2]] * 3, columns = ["label_one", "label_two"])
+    df_result = add_virtual_column(df, "label_one + label_two", "label3")
+    assert df_result.empty, f"7) Should return an empty df when the \"new_column\" is invalid.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
+    df = pd.DataFrame([[1, 2]] * 3, columns = ["label-one", "label_two"])
+    df_result = add_virtual_column(df, "label-one + label_two", "label")
+    assert df_result.empty, f"7) Should return an empty df when both df columns and roles are invalid.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
+    df = pd.DataFrame([[1, 2]] * 3, columns = ["label-one", "label_two"])
+    df_result = add_virtual_column(df, "label_one + label_two", "label")
+    assert df_result.empty, f"7) Should return an empty df when a df column is invalid.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
+    print("\n7) Test 'test_empty_result_when_invalid_labels_from_pdf' ✅")
+
+
+def test_empty_result_when_invalid_rules_from_pdf():
+    df = pd.DataFrame([[1, 1]] * 2, columns = ["label_one", "label_two"])
+    df_result = add_virtual_column(df, "label_one \\ label_two", "label_three")
+    assert df_result.empty, f"8) Should return an empty df when the role have invalid character: '\\'.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
+    df_result = add_virtual_column(df, "label&one + label_two", "label_three")
+    assert df_result.empty, f"8) Should return an empty df when the role have invalid character: '&'.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
+    df_result = add_virtual_column(df, "label_five + label_two", "label_three")
+    assert df_result.empty, f"8) Should return an empty df when the role have a column which isn't in the df: 'label_five'.\n\nResult:\n\n{df_result}\n\nExpected:\n\nEmpty df"
+    print("\n8) Test 'test_empty_result_when_invalid_rules_from_pdf' ✅")
+
 
 def test_when_extra_spaces_in_rules_from_pdf():
     df = pd.DataFrame([[1, 1]] * 2, columns = ["label_one", "label_two"])
@@ -87,7 +93,8 @@ def test_when_extra_spaces_in_rules_from_pdf():
     assert df_result.equals(df_expected), f"9) Should work when the role have extra spaces in the start/end.\n\nResult:\n\n{df_result}\n\nExpected:\n\n{df_expected}"
     print("\n9) Test 'test_when_extra_spaces_in_rules_from_pdf' ✅")
 
-#Below are unit tests that I added to check some additional edge-cases
+
+# -------------------- MY UNIT TESTS --------------------
 
 def test_edge_case_forbidden_operator():
     #Checks if the operators other than +, -, * are acceptes
@@ -95,6 +102,7 @@ def test_edge_case_forbidden_operator():
     result = add_virtual_column(df, "a / b", "res")
     assert result.empty, "10) Should return empty df, because '/' is not allowed."
     print("\n10) Test 'test_edge_case_forbidden_operator' ✅")
+
 
 def test_edge_case_missing_column_in_df():
     #Checks the behavior of the function when role refers to non-existent column
@@ -118,6 +126,7 @@ def test_edge_case_keyword_null_column():
     actual_val = result["total"].iloc[0] if not result.empty else None
     assert result.empty, "13) Should return empty df, because 'null' cannot be the name of the column."
     print("\n13) Test 'test_edge_case_keyword_null_column' ✅")
+
 
 def test_case_performance():
     rows = 100_000_000
